2700aec78d7835dba13cd932f3ca041e19cc0fc6
Kacper <kacperkozbial@onet.pl>
2017-08-11 22:30:47 +0200

    Main

---
+++ Main.java
@@ -0,0 +1,35 @@
+package com.esej.eHelp;
+ 
+import java.util.logging.Logger;
+
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.plugin.java.JavaPlugin;
+ 
+public class Main extends JavaPlugin
+{
+    Logger log = Logger.getLogger("Console");
+	private String tag;
+ 
+    public void onEnable()
+    {
+        log.info("[eHelp] Enabled.");
+    }
+ 
+    public void onDisable()
+    {
+        log.info("[eHelp] Disabled.");
+    }
+ 
+    public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args)
+    {
+        if (cmd.getName().equalsIgnoreCase("help"))
+        {
+        	tag = (ChatColor.DARK_GRAY + "[" + ChatColor.AQUA + "eHelp" + ChatColor.DARK_GRAY + "]");
+            sender.sendMessage(tag + ChatColor.RED + " Tu w przyszlosci bedzie skonfigurowana twoja wiadomosc!");
+            return true;
+        }
+        return false;
+    }
+}
